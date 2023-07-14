7ec41b1addabb1bfe2cd744436e2d0866d19c888
epinark <122348695+epinark@users.noreply.github.com>
2023-07-14 15:09:46 +0200

    edit input

--- src/components/MemeGenerator.jsx
+++ src/components/MemeGenerator.jsx
@@ -1,73 +1,76 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const MemeGenerator = () => {
-const [memePictures, setMemePictures] = useState([]);
-const [currentMeme, setCurrentMeme] = useState(null);
-const [topText, setTopText] = useState('');
-const [bottomText, setBottomText] = useState('');
+  const [memePictures, setMemePictures] = useState([]);
+  const [currentMeme, setCurrentMeme] = useState(null);
+  const [topText, setTopText] = useState("");
+  const [bottomText, setBottomText] = useState("");
 
-useEffect(() => {
+  useEffect(() => {
     const fetchMemePictures = async () => {
-        try {
-            const response = await axios.get('https://api.imgflip.com/get_memes');
-            setMemePictures(response.data.data.memes);
-            setCurrentMeme(response.data.data.memes[0]);
-        } catch (error) {
-            console.error(error);
-        }
+      try {
+        const response = await axios.get("https://api.imgflip.com/get_memes");
+        setMemePictures(response.data.data.memes);
+        setCurrentMeme(response.data.data.memes[2]);
+      } catch (error) {
+        console.error(error);
+      }
     };
 
     fetchMemePictures();
-}, []);
-return (
-    <div>
-         <input 
-      type="text"
-      placeholder="Enter top text"
-      value={topText}
-      onChange={(e) => setTopText(e.target.value)}
-   
-    />
-     <input
-      type="text"
-      placeholder="Enter bottom text"
-      value={bottomText}
-      onChange={(e) => setBottomText(e.target.value)}
-    />
-        {currentMeme && (
-      <div>
-        <img src={currentMeme.url} alt={currentMeme.name} />
-        <div
-          style={{
-            position: 'absolute',
-            top: '10px',
-            left: '10px',
-            color: 'white',
-            fontSize: '24px',
-            fontFamily:'impact',
-            textShadow: '1px 1px 1px black',
-          }}
-        >
-          {topText}
+  }, []);
+  return (
+    <div id="generatorWrapper">
+      <form id="generatorContainer">
+        <label htmlFor="topText">Text on the top: </label>
+        <input
+          type="text"
+          placeholder="Enter top text"
+          value={topText}
+          onChange={(e) => setTopText(e.target.value)}
+        />
+        <label htmlFor="bottomText">Text on the bottom: </label>
+        <input
+          type="text"
+          placeholder="Enter bottom text"
+          value={bottomText}
+          onChange={(e) => setBottomText(e.target.value)}
+        />
+      </form>
+      {currentMeme && (
+        <div>
+          <img src={currentMeme.url} alt={currentMeme.name} />
+          <div
+            style={{
+              position: "absolute",
+              top: "10px",
+              left: "10px",
+              color: "white",
+              fontSize: "24px",
+              fontFamily: "impact",
+              textShadow: "1px 1px 1px black",
+            }}
+          >
+            {topText}
+          </div>
+          <div
+            style={{
+              position: "absolute",
+              bottom: "10px",
+              left: "10px",
+              color: "white",
+              fontSize: "24px",
+              fontFamily: "impact",
+              textShadow: "1px 1px 1px black",
+            }}
+          >
+            {bottomText}
+          </div>
         </div>
-        <div
-          style={{
-            position: 'absolute',
-            bottom: '10px',
-            left: '10px',
-            color: 'white',
-            fontSize: '24px',
-            fontFamily:'impact',
-            textShadow: '1px 1px 1px black',
-          }}
-        >
-          {bottomText}
-        </div>
-      </div>
-    )}
-  </div>
-);
-}
+      )}
+    </div>
+  );
+};
 
 export default MemeGenerator;
